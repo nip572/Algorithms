0cbd9acd05364ab11c4f521bfa98830ce00362ad
nip572 <nipun.ahuja572@gmail.com>
2016-11-19 20:12:57 -0800

    Deepest Node and Merge Needed to make array palindromic

--- src/ctc_arrays_strings/MergeNeededToMakeArrayPalindrome.java
+++ src/ctc_arrays_strings/MergeNeededToMakeArrayPalindrome.java
@@ -1,7 +1,38 @@
-package ctc_arrays_strings;
+    package ctc_arrays_strings;
 
-/**
- * Created by Nipun on 11/19/16.
- */
-public class MergeNeededToMakeArrayPalindrome {
-}
+    /**
+     * Created by Nipun on 11/19/16.
+     */
+    public class MergeNeededToMakeArrayPalindrome {
+
+        public static int doOperation(int[] arr){
+
+            int start;
+            int end;
+            int ans = 0;
+            for(start = 0 , end= arr.length -1 ; start<= end ;){
+                if(arr[start] == arr[end]){
+                    start++;
+                    end--;
+                }
+                else if(arr[start] > arr[end]){
+                    end--;
+                    arr[end] += arr[end+1];
+                    ans++;
+                }
+                else{
+                    start++;
+                    arr[start] += arr[start -1];
+                    ans++;
+                }
+            }
+            return ans;
+        }
+
+        public static void main(String[] args){
+
+            int[] arr = {11, 14, 15, 99};
+            System.out.println(doOperation(arr));
+        }
+
+    }
